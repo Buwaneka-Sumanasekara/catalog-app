0402e56e634218d3c58e23f0ea0fbf40d319ca5b
Buwaneka Sumanasekara <buwanekasumanasekara@gmail.com>
2023-03-01 15:14:10 +0530

    added axios

---
+++ src/hooks/useFetchAnime.js
@@ -0,0 +1,15 @@
+import { getAxios } from "../utils/AxiosUtil";
+import Globals from "../constants/Globals"
+
+const api = getAxios()
+
+
+const getAnimeByStatus = async (status,page=0)=>{
+  return await api.get(`/anime`,{params:{
+    status:status,
+    page:page,
+    limit:Globals.ITEMS_PER_PAGE.HOME
+  }})
+}
+
+
---
+++ src/utils/AxiosUtil.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+
+export const getAxios = () => {
+    const axiosInstance = axios.create({
+      baseURL: process.env.API_URL,
+      timeout: process.env.DEFAULT_TIMEOUT
+        ? parseInt(process.env.DEFAULT_TIMEOUT)
+        : 0,
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    })
+  
+    return axiosInstance
+}
